246caff519e489118ba2995d9f7bd882c3f6c60f
wuhl508159280 <32049569+wuhl508159280@users.noreply.github.com>
2023-12-27 10:32:25 +0800

    Add files via upload

---
+++ LogService.cs
@@ -0,0 +1,13 @@
+﻿using NLog;
+using NLog.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoteDataManager.Services
+{
+    public static class LogService
+    {
+        public static readonly Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+    }
+}
